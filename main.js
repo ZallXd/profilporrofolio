6c768e741e3374f4edad012c58317fd05e0aade3
ZallXd <100450216+ZallXd@users.noreply.github.com>
2023-03-31 10:26:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var audio=new Audio("https://x2convert.video/id/Thankyou?token=U2FsdGVkX18YlR%2fTgwvm%2f1IIUSlkUG7ZELBH7oUJg%2btbzUK%2b7uKbn4mIy0%2bvyb%2fHkkho%2bqBOViwYkKBLE568IqyMIE1prYpYBago%2fxoS5M4uSaBAEedZ0MDf7JEddVLkMUmL5BZgJiOgy8sI9NIC6lgoHpmp9T1TbOrKbGW%2bQg7yFYpx5KI5Stzkrx0uWlKptISeZe9P83HFuV2FAC20GrN2OP8ng95W7%2f2pu0eOllU%3d&s=youtube&id=&h=3606243479182093620"),playing=false;
+var audio=new Audio("https://x2convert.video/id/Thankyou?token=U2FsdGVkX19TqO3BSSKy5t5dJRxwbbVv9dnIqNA8VGlYHN%2f7yqUBao4D%2bpQrIe4p%2bGlH%2b4%2fuwVFLgtX%2bNlautM9LVrZpVctogxzqvWtWndcDFbxIVuPF%2f7YVD%2bmBHZR%2fZFnKJT9w1pUbQ113JwZAs2Iif%2fqG9i%2fmSqA1vSqWR%2fwlzg3PIhgCVydiaPsZPpaPPEqHTALOp6jtGSOpeoZf1kL9hHiHxIez%2fZ22HyNioEM%3d&s=youtube&id=&h=7293937682390376117"),playing=false;
 
    audio.currentTime=160;
 
